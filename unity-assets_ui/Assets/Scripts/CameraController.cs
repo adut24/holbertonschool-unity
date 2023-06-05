d9cfa8cdace0153b5eab82d3a3dabd3c786b564b
adut24 <dutertre.alexandre@laposte.net>
2023-06-05 13:52:25 +0200

    Add prefab for the win canvas

--- unity-assets_ui/Assets/Scripts/CameraController.cs
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -10,8 +10,8 @@ public class CameraController : MonoBehaviour
     private Vector3 velocity;
     [SerializeField]
     private float mouseSensitivity = 3f;
-    private float rotationY;
     private float rotationX;
+    private float rotationY;
     public bool isInverted;
 
     private void Awake() => DontDestroyOnLoad(gameObject);
@@ -22,25 +22,28 @@ public class CameraController : MonoBehaviour
         {
             player = GameObject.FindWithTag("Player").GetComponent<Transform>();
             transform.position = player.position + posOffset;
-            transform.eulerAngles = new(9, 0, 0);
         }
         if (player)
         {
             transform.position = Vector3.SmoothDamp(transform.position, player.position + posOffset, ref velocity, timeOffset);
-
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-            float mouseY = isInverted ? -Input.GetAxis("Mouse Y") * mouseSensitivity : Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-            rotationY += mouseX;
-            rotationX += mouseY;
-
-            rotationX = Mathf.Clamp(rotationX, -20, 40);
-            rotationY = Mathf.Clamp(rotationY, -70, 70);
-
-            transform.LookAt(player);
-            transform.RotateAround(player.position, new Vector3(1, 0, 0), rotationX);
-            transform.RotateAround(player.position, new Vector3(0, 1, 0), rotationY);
-            player.forward = transform.forward;
+            RotateCamera();
         }
+    }
+
+    private void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = isInverted ? -Input.GetAxis("Mouse Y") * mouseSensitivity : Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationY += mouseX;
+        rotationX += mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, -20, 40);
+        rotationY = Mathf.Clamp(rotationY, -70, 70);
+
+        transform.LookAt(player);
+        transform.RotateAround(player.position, new Vector3(1, 0, 0), rotationX);
+        transform.RotateAround(player.position, new Vector3(0, 1, 0), rotationY);
+        player.forward = transform.forward;
     }
 }
